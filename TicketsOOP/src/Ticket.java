1c168f936d83d49a95fd1d3b31bae2482e192dd4
Nick Toffle <Stea1kWaffle@gmail.com>
2015-03-30 22:01:16 -0500

    made changes and added options

--- TicketsOOP/src/Ticket.java
+++ TicketsOOP/src/Ticket.java
@@ -5,6 +5,7 @@ public class Ticket {
 	private String reporter; //Stores person or department who reported issue
 	private String description;
 	private Date dateReported;
+	private boolean resolved;
 	
 	//STATIC Counter - accessible to all Ticket objects.
 	//If any Ticket object modifies this counter, all Ticket objects will have the modified value
@@ -13,11 +14,24 @@ public class Ticket {
 	//The ID for each ticket - instance variable. Each Ticket will have it's own ticketID variable
 	protected int ticketID;
 	
+	protected boolean getResolved(){
+		return this.resolved;
+	}
+	
+	protected void setResolvedTrue(){
+		this.resolved = true;
+	}
+	
+	protected void setResolvedFalse(){
+		this.resolved = false;
+	}
+	
 	public Ticket(String desc, int p, String rep, Date date) {
 		this.description = desc;
 		this.priority = p;
 		this.reporter = rep; 
-		this.dateReported = date;		
+		this.dateReported = date;
+		this.resolved = false;
 		this.ticketID = staticTicketIDCounter; 
 		staticTicketIDCounter++;
 	}
--- TicketsOOP/src/ticketGUI.java
+++ TicketsOOP/src/ticketGUI.java
@@ -23,6 +23,7 @@ import javax.swing.ListSelectionModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Date;
+import javax.swing.JCheckBox;
 
 
 public class ticketGUI extends JFrame {
@@ -50,6 +51,8 @@ public class ticketGUI extends JFrame {
 
 	DefaultListModel<Ticket> ticketListModel;
 	DefaultListModel<Ticket> ticket2;
+	private GridBagConstraints gbc_listResolvedTickets;
+	DefaultListModel<Ticket> ResolvedListModel;
 	
 	/**
 	 * Create the frame.
@@ -57,114 +60,36 @@ public class ticketGUI extends JFrame {
 	public ticketGUI() {
 		super("List of Tickets");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 468);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		ticketListModel = new DefaultListModel<Ticket>();
+		ResolvedListModel = new DefaultListModel<Ticket>();
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 187, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblTicketSystem = new JLabel("Ticket System");
 		GridBagConstraints gbc_lblTicketSystem = new GridBagConstraints();
 		gbc_lblTicketSystem.insets = new Insets(0, 0, 5, 5);
-		gbc_lblTicketSystem.gridx = 1;
+		gbc_lblTicketSystem.gridx = 0;
 		gbc_lblTicketSystem.gridy = 0;
 		contentPane.add(lblTicketSystem, gbc_lblTicketSystem);
-
-		final JList ticketJList = new JList();
-		GridBagConstraints gbc_list = new GridBagConstraints();
-		gbc_list.insets = new Insets(0, 0, 0, 5);
-		gbc_list.fill = GridBagConstraints.BOTH;
-		gbc_list.gridx = 3;
-		gbc_list.gridy = 7;
-		contentPane.add(ticketJList, gbc_list);
-
-		JButton btnAddTicket = new JButton("Add Ticket");
 		
+		JButton btnAddTicket = new JButton("Add Ticket");
+				
 		GridBagConstraints gbc_btnAddTicket = new GridBagConstraints();
-		gbc_btnAddTicket.insets = new Insets(0, 0, 5, 5);
-		gbc_btnAddTicket.gridx = 1;
+		gbc_btnAddTicket.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAddTicket.gridx = 2;
 		gbc_btnAddTicket.gridy = 2;
 		contentPane.add(btnAddTicket, gbc_btnAddTicket);
-		
-		JLabel lblProblemDescription = new JLabel("Problem Description");
-		GridBagConstraints gbc_lblProblemDescription = new GridBagConstraints();
-		gbc_lblProblemDescription.insets = new Insets(0, 0, 5, 5);
-		gbc_lblProblemDescription.anchor = GridBagConstraints.EAST;
-		gbc_lblProblemDescription.gridx = 1;
-		gbc_lblProblemDescription.gridy = 3;
-		contentPane.add(lblProblemDescription, gbc_lblProblemDescription);
-		
-		textFieldPD = new JTextField();
-		textFieldPD.setText(" ");
-		GridBagConstraints gbc_textFieldPD = new GridBagConstraints();
-		gbc_textFieldPD.insets = new Insets(0, 0, 5, 5);
-		gbc_textFieldPD.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textFieldPD.gridx = 3;
-		gbc_textFieldPD.gridy = 3;
-		contentPane.add(textFieldPD, gbc_textFieldPD);
-		textFieldPD.setColumns(10);
-		
-		JLabel labelReporter = new JLabel("Problem Reporter?");
-		GridBagConstraints gbc_labelReporter = new GridBagConstraints();
-		gbc_labelReporter.anchor = GridBagConstraints.EAST;
-		gbc_labelReporter.insets = new Insets(0, 0, 5, 5);
-		gbc_labelReporter.gridx = 1;
-		gbc_labelReporter.gridy = 4;
-		contentPane.add(labelReporter, gbc_labelReporter);
-		
-		txtReporter = new JTextField();
-		GridBagConstraints gbc_txtReporter = new GridBagConstraints();
-		gbc_txtReporter.insets = new Insets(0, 0, 5, 5);
-		gbc_txtReporter.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtReporter.gridx = 3;
-		gbc_txtReporter.gridy = 4;
-		contentPane.add(txtReporter, gbc_txtReporter);
-		txtReporter.setColumns(10);
-		
-		JLabel lblPriority = new JLabel("Priority (20 pt scale)");
-		GridBagConstraints gbc_lblPriority = new GridBagConstraints();
-		gbc_lblPriority.anchor = GridBagConstraints.EAST;
-		gbc_lblPriority.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPriority.gridx = 1;
-		gbc_lblPriority.gridy = 5;
-		contentPane.add(lblPriority, gbc_lblPriority);
-		
-		txtPriority = new JTextField();
-		GridBagConstraints gbc_txtPriority = new GridBagConstraints();
-		gbc_txtPriority.insets = new Insets(0, 0, 5, 5);
-		gbc_txtPriority.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtPriority.gridx = 3;
-		gbc_txtPriority.gridy = 5;
-		contentPane.add(txtPriority, gbc_txtPriority);
-		txtPriority.setColumns(10);
-		
-		JLabel lblCurrentTickets = new JLabel("Current Tickets");
-		GridBagConstraints gbc_lblCurrentTickets = new GridBagConstraints();
-		gbc_lblCurrentTickets.insets = new Insets(0, 0, 5, 5);
-		gbc_lblCurrentTickets.gridx = 3;
-		gbc_lblCurrentTickets.gridy = 6;
-		contentPane.add(lblCurrentTickets, gbc_lblCurrentTickets);
-		
-		JButton btnDeleteTicket = new JButton("Delete Ticket");
-		
-		GridBagConstraints gbc_btnDeleteTicket = new GridBagConstraints();
-		gbc_btnDeleteTicket.insets = new Insets(0, 0, 0, 5);
-		gbc_btnDeleteTicket.gridx = 1;
-		gbc_btnDeleteTicket.gridy = 7;
-		contentPane.add(btnDeleteTicket, gbc_btnDeleteTicket);
-		
-		ticketJList.setModel(ticketListModel);
-		
-		ticketJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
+				
 		btnAddTicket.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -184,14 +109,135 @@ public class ticketGUI extends JFrame {
 			}
 		});
 		
+		JLabel lblCurrentTickets = new JLabel("Current Tickets");
+		GridBagConstraints gbc_lblCurrentTickets = new GridBagConstraints();
+		gbc_lblCurrentTickets.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCurrentTickets.gridx = 0;
+		gbc_lblCurrentTickets.gridy = 7;
+		contentPane.add(lblCurrentTickets, gbc_lblCurrentTickets);
+		
+		JLabel lblUnresolved = new JLabel("Unresolved");
+		GridBagConstraints gbc_lblUnresolved = new GridBagConstraints();
+		gbc_lblUnresolved.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUnresolved.gridx = 0;
+		gbc_lblUnresolved.gridy = 9;
+		contentPane.add(lblUnresolved, gbc_lblUnresolved);
+		
+		JLabel lblResolved = new JLabel("Resolved");
+		GridBagConstraints gbc_lblResolved = new GridBagConstraints();
+		gbc_lblResolved.insets = new Insets(0, 0, 5, 0);
+		gbc_lblResolved.gridx = 2;
+		gbc_lblResolved.gridy = 9;
+		contentPane.add(lblResolved, gbc_lblResolved);
+		
+		JLabel lblProblemDescription = new JLabel("Problem Description");
+		GridBagConstraints gbc_lblProblemDescription = new GridBagConstraints();
+		gbc_lblProblemDescription.insets = new Insets(0, 0, 5, 5);
+		gbc_lblProblemDescription.anchor = GridBagConstraints.EAST;
+		gbc_lblProblemDescription.gridx = 0;
+		gbc_lblProblemDescription.gridy = 3;
+		contentPane.add(lblProblemDescription, gbc_lblProblemDescription);
+		
+		textFieldPD = new JTextField();
+		textFieldPD.setText(" ");
+		GridBagConstraints gbc_textFieldPD = new GridBagConstraints();
+		gbc_textFieldPD.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldPD.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldPD.gridx = 2;
+		gbc_textFieldPD.gridy = 3;
+		contentPane.add(textFieldPD, gbc_textFieldPD);
+		textFieldPD.setColumns(10);
+		
+		JLabel labelReporter = new JLabel("Problem Reporter?");
+		GridBagConstraints gbc_labelReporter = new GridBagConstraints();
+		gbc_labelReporter.anchor = GridBagConstraints.EAST;
+		gbc_labelReporter.insets = new Insets(0, 0, 5, 5);
+		gbc_labelReporter.gridx = 0;
+		gbc_labelReporter.gridy = 4;
+		contentPane.add(labelReporter, gbc_labelReporter);
+		
+		txtReporter = new JTextField();
+		GridBagConstraints gbc_txtReporter = new GridBagConstraints();
+		gbc_txtReporter.insets = new Insets(0, 0, 5, 0);
+		gbc_txtReporter.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtReporter.gridx = 2;
+		gbc_txtReporter.gridy = 4;
+		contentPane.add(txtReporter, gbc_txtReporter);
+		txtReporter.setColumns(10);
+		
+		JLabel lblPriority = new JLabel("Priority (20 pt scale)");
+		GridBagConstraints gbc_lblPriority = new GridBagConstraints();
+		gbc_lblPriority.anchor = GridBagConstraints.EAST;
+		gbc_lblPriority.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPriority.gridx = 0;
+		gbc_lblPriority.gridy = 5;
+		contentPane.add(lblPriority, gbc_lblPriority);
+		
+		txtPriority = new JTextField();
+		GridBagConstraints gbc_txtPriority = new GridBagConstraints();
+		gbc_txtPriority.insets = new Insets(0, 0, 5, 0);
+		gbc_txtPriority.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPriority.gridx = 2;
+		gbc_txtPriority.gridy = 5;
+		contentPane.add(txtPriority, gbc_txtPriority);
+		txtPriority.setColumns(10);
+		
+		final JList ticketJList = new JList();
+		GridBagConstraints gbc_listResolved = new GridBagConstraints();
+		gbc_listResolved.insets = new Insets(0, 0, 5, 5);
+		gbc_listResolved.fill = GridBagConstraints.BOTH;
+		gbc_listResolved.gridx = 0;
+		gbc_listResolved.gridy = 10;
+		contentPane.add(ticketJList, gbc_listResolved);
+				
+		ticketJList.setModel(ticketListModel);
+			
+		ticketJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		final JList listResolvedTickets = new JList();
+		GridBagConstraints gbc_listResolvedTickets;
+		gbc_listResolvedTickets = new GridBagConstraints();
+		gbc_listResolvedTickets.insets = new Insets(0, 0, 5, 0);
+		gbc_listResolvedTickets.fill = GridBagConstraints.BOTH;
+		gbc_listResolvedTickets.gridx = 2;
+		gbc_listResolvedTickets.gridy = 10;
+		contentPane.add(listResolvedTickets, gbc_listResolvedTickets);
+
+		listResolvedTickets.setModel(ResolvedListModel);
+		
+		JButton btnResolveTicket = new JButton("Resolve Ticket");
+		
+		GridBagConstraints gbc_btnResolveTicket = new GridBagConstraints();
+		gbc_btnResolveTicket.insets = new Insets(0, 0, 0, 5);
+		gbc_btnResolveTicket.gridx = 0;
+		gbc_btnResolveTicket.gridy = 11;
+		contentPane.add(btnResolveTicket, gbc_btnResolveTicket);
+		
+		JButton btnDeleteTicket = new JButton("Delete Resolved Ticket");
+		
+		GridBagConstraints gbc_btnDeleteTicket = new GridBagConstraints();
+		gbc_btnDeleteTicket.gridx = 2;
+		gbc_btnDeleteTicket.gridy = 11;
+		contentPane.add(btnDeleteTicket, gbc_btnDeleteTicket);
+		
+		btnResolveTicket.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ticket2 = (DefaultListModel)ticketJList.getModel();
+				Ticket resolvedTicket = ticket2.getElementAt(ticketJList.getSelectedIndex());
+				resolvedTicket.setResolvedTrue();
+				ticketGUI.this.ResolvedListModel.addElement(resolvedTicket);
+				ticketGUI.this.ticketListModel.removeElement(resolvedTicket);
+			}
+		});
+		
 		btnDeleteTicket.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e){
-				ticket2 = (DefaultListModel)ticketJList.getModel();
-				Ticket toDelete = ticket2.getElementAt(ticketJList.getSelectedIndex());
-				ticketGUI.this.ticketListModel.removeElement(toDelete);
+				ticket2 = (DefaultListModel)listResolvedTickets.getModel();
+				Ticket toDelete = ticket2.getElementAt(listResolvedTickets.getSelectedIndex());
+				ticketGUI.this.ResolvedListModel.removeElement(toDelete);
 			}
 		});
 	}
-
 }
